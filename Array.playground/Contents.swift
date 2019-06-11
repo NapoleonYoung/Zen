7cb3dbaf3ee153d2a30d1da462473be313839873
NapoleonYoung <qq906469826@163.com>
2019-06-11 22:24:08 +0800

    array:数组中数字出现的次数

--- Array.playground/Contents.swift
+++ Array.playground/Contents.swift
@@ -31,3 +31,43 @@ func findGreatestSumOfSubArray(destinatedArray: [Int]?) -> Int {
 }
 
 let a = findGreatestSumOfSubArray(destinatedArray: [1, -2, 3, 10, -4, 7, 2, -5])
+
+/*
+ 2.
+ title: 数组中数字出现的次数
+ from：剑指 Offer 56 题
+ description: 一个整型数组里除两个数字之外，其他数字都出现了两次。请写程序找出这两个只出现一次的数字。
+ 要求时间复杂度是 O(n)，空间复杂度是 O(1)。
+ */
+func findNumsAppearOnce(desArray: [Int]) -> [Int] {
+    var numOne: Int = 0
+    var numTwo: Int = 0
+    guard desArray.count > 1 else {
+        return [numOne, numTwo]
+    }
+    // 1. 求出数组中所有数字的异或结果
+    var exclusizeOr: Int = 0
+    for index in 0..<desArray.count {
+        exclusizeOr ^= desArray[index]
+    }
+    // 2. 找到异或结果中第一个 1 的位置
+    var indexOfBits: Int = 0
+    while exclusizeOr % 2 == 0 {
+        indexOfBits += 1
+    }
+    // 2. 根据异或结果，按照数组中某一位为 1 或者 0 分为两组，每组异或结果即为要找的数
+    
+    for item in desArray {
+        if ((item >> indexOfBits) & 1) == 1 {
+            numOne ^= item
+        } else {
+            numTwo ^= item
+        }
+    }
+    return [numOne, numTwo]
+}
+//let testArray = [1, 1, 2, 2, 3, 4, 4, 5, 5, 6]
+let testArray = [1]
+findNumsAppearOnce(desArray: testArray)
+
+
